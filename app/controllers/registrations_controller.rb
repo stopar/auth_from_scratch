05c11a9ca4f7577a051c04be9e01cfbace13274d
Cezar Halmagean <cezar@mixandgo.com>
2023-08-10 09:29:53 +0300

    Added Registrations#create

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,19 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(create_params)
+
+    if @user.save
+      flash[:notice] = "Welcome #{@user.email}"
+      redirect_to root_path
+    end
+  end
+
+  private
+
+    def create_params
+      params.require(:user).permit(:email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   get "/registration", to: "registrations#new", as: :sign_up
+  post "/registration", to: "registrations#create", as: :registration
   root "site#index"
 end
